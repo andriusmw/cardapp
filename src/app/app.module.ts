ca7528dc98771d60b21e10b43b7e85b8067eab7f
andriusmw <andriusmw@yahoo.es>
2022-03-03 13:40:37 +0100

    nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { PhotoPreviewComponent } from './photo-preview/photo-preview.component';
+import { PhotoListComponent } from './photo-list/photo-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    PhotoFormComponent,
+    PhotoPreviewComponent,
+    PhotoListComponent,
 
   ],
   imports: [
---
+++ src/app/photo-preview/photo-preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-preview',
+  templateUrl: './photo-preview.component.html',
+  styleUrls: ['./photo-preview.component.css']
+})
+export class PhotoPreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
