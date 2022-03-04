231579de514d7b9b34df8504e4fb4481b4cea474
andriusmw <andriusmw@yahoo.es>
2022-03-04 14:57:42 +0100

    meotodo para enviar la foto cargada al servidor

--- src/app/services/photo-service.service.ts
+++ src/app/services/photo-service.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoServiceService {
 
-  constructor() { }
+  URI= "http://localhost:4000/photos"
+
+  constructor(private http: HttpClient) { }
+
+  createPhoto(title: string, description: string, photo: File){
+     const fd = new FormData();
+     fd.append("title", title);
+     //A la constante fd, de tipo FormData le añade el campo
+     //title con el valor title.
+     fd.append("description", description);
+     fd.append("image", photo);
+     return this.http.post(this.URI, fd);
+  }
+
+  //Esto es para enviar los datos de la foto cargada al servidor
+  //que luego se mostrará en la pestaña photos.
+  //Hay que añadir el servidor y levantarlo
 }
