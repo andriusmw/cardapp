8268fc0735bafd1a21a1d3f80afcd12d5d0cf740
andriusmw <andriusmw@yahoo.es>
2022-03-07 15:23:07 +0100

    carga imagen en photopreview

--- src/app/photo-preview/photo-preview.component.ts
+++ src/app/photo-preview/photo-preview.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Router,ActivatedRoute} from "@angular/router"
+import { PhotoServiceService } from '../services/photo-service.service';
+import {Photo} from "../interfaces/Photo"
 
 @Component({
   selector: 'app-photo-preview',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./photo-preview.component.css']
 })
 export class PhotoPreviewComponent implements OnInit {
+  id: string;
+  photo: Photo;
 
-  constructor() { }
+
+  constructor(
+     private router: Router,
+     private activatedroute: ActivatedRoute,
+     private photoservice: PhotoServiceService
+     ) { }
 
   ngOnInit(): void {
+    this.activatedroute.params.subscribe(params => {
+     this.id = params["id"];
+     //Crea una variable llamada id y le pasa el valor de la "id"
+     //que hay en la url
+     this.photoservice.getPhoto(this.id)
+      .subscribe(
+        res => {
+          this.photo = res;
+        },
+        err => console.log(err)
+      )
+    })
   }
 
 }
